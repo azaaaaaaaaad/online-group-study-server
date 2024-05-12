021d82085da6c03821fdb39ea3aa84028a4c1aad
jrazad10 <jrazad10@gmail.com>
2024-05-12 13:55:01 +0600

    create assignment post created in server side

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-const corsOption= {
+const corsOption = {
     origin: ['http://localhost:5173'],
     credentials: true,
     optionSuccessStatus: 200,
@@ -14,10 +15,48 @@ const corsOption= {
 app.use(cors(corsOption));
 app.use(express.json());
 
-app.get('/', (req,res)=> {
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdffxhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        const featuresCollection = client.db('GroupStudy').collection('feature')
+        const assignmentsCollection = client.db('GroupStudy').collection('assignments')
+
+
+        //get features to homepage from db
+        app.get('/feature', async (req, res) => {
+            const result = await featuresCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        //save assignments in db
+        app.post('/assignments', async (req, res) => {
+            const assignment = req.body
+            const result = await assignmentsCollection.insertOne(assignment)
+            res.send(result)
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Online group study began')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Online group study began at ${port} `);
 })
