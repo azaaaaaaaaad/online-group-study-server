321be85580532ad795f0748876ee2d2b521aa556
jrazad10 <jrazad10@gmail.com>
2024-05-12 19:50:41 +0600

    getting individual data from assignments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,19 @@ async function run() {
         app.post('/assignments', async (req, res) => {
             const assignment = req.body
             const result = await assignmentsCollection.insertOne(assignment)
+            res.send(result)
+        })
+        //get assignments from db
+        app.get('/assignments', async (req, res) => {
+            const result = await assignmentsCollection.find().toArray();
+            res.send(result)
+        })
+
+        //get individual assignment from db
+        app.get('/assignments/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await assignmentsCollection.findOne(query);
             res.send(result)
         })
 
